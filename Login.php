6fac6d87ec8b621671f5fdaf781622cc9256a223
marsh189 <marsh189@mail.chapman.edu>
2018-04-30 14:51:26 -0700

    added php

---
+++ Login.php
@@ -0,0 +1,27 @@
+<?php
+    $con = mysqli_connect("den1.mysql2.gear.host", "giftgiving", "Wf6s7G!!sASh", "giftgiving");
+    
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    $statement = mysqli_prepare($con, "SELECT * FROM Users WHERE username = ? AND password = ?");
+    mysqli_stmt_bind_param($statement, "ss", $username, $password);
+    mysqli_stmt_execute($statement);
+    
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement, $name, $email, $username, $password, $profile_pic);
+    
+    $response = array();
+    $response["success"] = false;  
+    
+    while(mysqli_stmt_fetch($statement)){
+        $response["success"] = true;  
+        $response["username"] = $username;
+        $response["password"] = $password;
+        $response["name"] = $name;
+        $response["email"] = $email;
+        $response["profile_pic"] = $profile_pic;
+    }
+    
+    echo json_encode($response);
+?>
