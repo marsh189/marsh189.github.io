2ac31f516d3fa51689223224d91e869e70364c9b
marsh189 <marsh189@mail.chapman.edu>
2018-04-30 17:01:37 -0700

    fixed php

--- Login.php
+++ Login.php
@@ -4,24 +4,20 @@
     $username = $_POST["username"];
     $password = $_POST["password"];
     
-    $statement = mysqli_prepare($con, "SELECT * FROM users WHERE username = ? AND password = ?");
-    mysqli_stmt_bind_param($statement, "ss", $username, $password);
+    $statement = mysqli_prepare($con, "SELECT * FROM users WHERE username = ?");
+    mysqli_stmt_bind_param($statement, "s", $username);
     mysqli_stmt_execute($statement);
-    
     mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $name, $email, $username, $password, $profile_pic);
+    mysqli_stmt_bind_result($statement, $colUsername, $colPassword, $colName, $email);
     
     $response = array();
     $response["success"] = false;  
     
     while(mysqli_stmt_fetch($statement)){
-        $response["success"] = true;  
-        $response["username"] = $username;
-        $response["password"] = $password;
-        $response["name"] = $name;
-        $response["email"] = $email;
-        $response["profile_pic"] = $profile_pic;
+        if (password_verify($password, $colPassword)) {
+            $response["success"] = true;  
+            $response["name"] = $colName;
+        }
     }
-    
     echo json_encode($response);
 ?>
